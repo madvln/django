3411004fa7edbac95eb5d2afc378607f2eb84742
madvln <madvln@yandex.ru>
2024-08-29 11:54:55 +0000

    lesson-36

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(\
-            is_published=Women.Status.PUBLISHED)
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
 class Women(models.Model):
@@ -18,10 +17,15 @@ class Women(models.Model):
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices,\
-         default=Status.DRAFT)
+    is_published = models.BooleanField(choices=Status.choices, \
+        default=Status.DRAFT)
+    cat = models.ForeignKey("Category", on_delete=models.CASCADE, \
+        related_name="posts")
+    tags = models.ManyToManyField("TagPost", blank=True, related_name="tags")
     objects = models.Manager()
     published = PublishedManager()
+    husband = models.OneToOneField("Husband", on_delete=models.SET_NULL, \
+        null=True, blank=True, related_name='wuman')
 
     def __str__(self):
         return self.title
@@ -32,3 +36,33 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_slug": self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_slug": self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
+    
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+    m_count = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.name
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,46 +2,13 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 
-from .models import Women
+from .models import Women, Category, TagPost
 
 menu = [
     {"title": "О сайте", "url_name": "about"},
     {"title": "Добавить статью", "url_name": "add_page"},
     {"title": "Контакты", "url_name": "contact"},
     {"title": "Вход", "url_name": "login"},
-]
-
-data_db = [
-    {
-        "id": 1,
-        "title": "Актриса 1",
-        "content": """<h1>zz</h1> zzz zz zzz z zzzz zzzzz zzz zzz zz zzz zz zz zz z
-        zzzzz zzzzz zzz zzz zz zz zz zz zzz zzzzz
-        zz zzz zzzz zzzz zzzzz zzz zzzz zzzz z zzz zzz zzz zz zz zzzz zzzzz zzz
-        zzzzz zzzzz zzz zzz zz zz zz zz zzz zzzzz 
-        zzzzz zzzzz zzz zzz zz zz zz zz zzz zzzzz
-        zzzzzzzzzzz""",
-        "is_published": True,
-    },
-    {
-        "id": 2,
-        "title": "Актриса 2",
-        "content": "Биография актрисы 2",
-        "is_published": False,
-    },
-    {
-        "id": 3,
-        "title": "Актриса 3",
-        "content": "Биография актрисы 3",
-        "is_published": True,
-    },
-]
-
-
-cats_db = [
-    {"id": 1, "name": "Актрисы"},
-    {"id": 2, "name": "Певицы"},
-    {"id": 3, "name": "Спортсменки"},
 ]
 
 
@@ -85,15 +52,29 @@ def login(request):
     return HttpResponse("Login")
 
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Women.published.filter(cat_id=category.pk)
     data = {
-        "title": "Отопражение по рубрикам",
+        "title": f"Рубрика: {category.name}",
         "menu": menu,
-        "posts": data_db,
-        "cat_selected": cat_id,
+        "posts": posts,
+        "cat_selected": category.pk,
     }
     return render(request, "women/index.html", context=data)
 
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Page not found</h1>")
+
+
+def show_tags_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+    data = {
+        "title": f"Тег: {tag.tag}",
+        "menu": menu,
+        "posts": posts,
+        "cat_selected": None,
+    }
+    return render(request, "women/index.html", context=data)
