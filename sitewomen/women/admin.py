62b1bd3f592895ea77649b8db06213085c65040c
madvln <madvln@yandex.ru>
2024-09-02 13:11:16 +0000

    admin-final 42

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -2,15 +2,35 @@ from django.contrib import admin, messages
 from .models import Women, Category
 
 
+class MariedFilter(admin.SimpleListFilter):
+    title = "Статус женщин"
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("married", "Замужем"),
+            ("single", "Не замужем"),
+            ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "married":
+            return queryset.filter(husband__isnull=False)
+        if self.value() == "single":
+            return queryset.filter(husband__isnull=True)
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ("title", "time_create",
-        "is_published", "cat", "brief_info")
+    fields = ["title", "slug", "content", "cat", "husband", "tags"]
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ["tags"]
+    list_display = ("title", "time_create", "is_published", "cat", "brief_info")
     list_display_links = ("title",)
     ordering = ["-time_create", "title"]
     list_editable = ("is_published",)
     list_per_page = 5
     actions = ["set_published", "set_draft"]
+    search_fields = ["title__startswith", "cat__name"]
+    list_filter = [MariedFilter, "cat__name", "is_published"]
 
     @admin.display(description="Краткое описание", ordering="content")
     def brief_info(self, women: Women):
@@ -24,8 +44,9 @@ class WomenAdmin(admin.ModelAdmin):
     @admin.action(description="Снять с публикации")
     def set_draft(self, request, queryset):
         count = queryset.update(is_published=Women.Status.DRAFT)
-        self.message_user(request, f"{count} записей снято с публикации.",
-            messages.WARNING)
+        self.message_user(
+            request, f"{count} записей снято с публикации.", messages.WARNING
+        )
 
 
 @admin.register(Category)
