fac9d943e13a6b8ce921edd8cb130c37424db312
madvln <madvln@yandex.ru>
2024-09-09 08:27:48 +0000

    lesson-55

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("addpage/", views.AddPage.as_view(), name="add_page"),
     path("contact/", views.contact, name="contact"),
     path("login/", views.login, name="login"),
-    path("post/<slug:post_slug>/", views.show_post, name="post"),
+    path("post/<slug:post_slug>/", views.ShowPost.as_view(), name="post"),
     path("category/<slug:cat_slug>/", views.WomenCategory.as_view(), name="category"),
-    path("tag/<slug:tag_slug>/", views.WomenTags.as_view(), name="tag"),
+    path("tag/<slug:tag_slug>/", views.TagPostList.as_view(), name="tag"),
+    path("edit/<slug:slug>/", views.UpdatePage.as_view(), name="edit_page"),
+    path("delete/<slug:slug>/", views.DeletePage.as_view(), name="delete_page"),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils.text import slugify
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.conf import settings
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost, UploadFiles
@@ -18,7 +24,6 @@ menu = [
 
 
 class WomenHome(ListView):
-    # model = Women
     template_name = "women/index.html"
     context_object_name = "posts"
     extra_context = {
@@ -51,40 +56,50 @@ def about(request):
     )
 
 
-def show_post(request, post_slug):
-    post = Women.published.get(slug=post_slug)
+class ShowPost(DetailView):
+    template_name = "women/post.html"
+    slug_url_kwarg = "post_slug"
+    context_object_name = "post"
 
-    data = {
-        "title": post.title,
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["post"].title
+        context["menu"] = menu
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
+
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = "women/addpage.html"
+    extra_context = {
+        "title": "Добавление статьи",
         "menu": menu,
-        "post": post,
-        "cat_selected": 1,
     }
 
-    return render(request, "women/post.html", data)
+
+class UpdatePage(UpdateView):
+    model = Women
+    fields = ["title", "content", "photo", "is_published", "cat"]
+    template_name = "women/addpage.html"
+    success_url = reverse_lazy("home")
+    extra_context = {
+        "menu": menu,
+        "title": "Редактирование статьи",
+    }
 
 
-class AddPage(View):
-    def get(self, request):
-        form = AddPostForm()
-        data = {
-            "menu": menu,
-            "title": "Добавление статьи",
-            "form": form,
-        }
-        return render(request, "women/addpage.html", data)
-
-    def post(self, request):
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-        data = {
-            "menu": menu,
-            "title": "Добавление статьи",
-            "form": form,
-        }
-        return render(request, "women/addpage.html", data)
+class DeletePage(DeleteView):
+    model = Women
+    context_object_name = "post"
+    template_name = "women/delete_confirm.html"
+    success_url = reverse_lazy("home")
+    extra_context = {
+        "menu": menu,
+        "title": "Удаление статьи",
+    }
 
 
 def contact(request):
@@ -95,32 +110,22 @@ def login(request):
     return HttpResponse("Login")
 
 
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = Women.published.select_related("cat")
-#     data = {
-#         "title": f"Рубрика: {category.name}",
-#         "menu": menu,
-#         "posts": posts,
-#         "cat_selected": category.pk,
-#     }
-#     return render(request, "women/index.html", context=data)
-
-
 class WomenCategory(ListView):
     template_name = "women/index.html"
     context_object_name = "posts"
     allow_empty = False
 
     def get_queryset(self):
-        return Women.published.filter(cat__slug=self.kwargs["cat_slug"]).select_related("cat")
-    
+        return Women.published.filter(cat__slug=self.kwargs["cat_slug"]).select_related(
+            "cat"
+        )
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context["posts"][0].cat
         context["title"] = "Категория - " + cat.name
-        context["menu"] = menu 
-        context["cat_selected"] = cat.pk 
+        context["menu"] = menu
+        context["cat_selected"] = cat.pk
         return context
 
 
@@ -128,18 +133,20 @@ def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Page not found</h1>")
 
 
-class WomenTags(ListView):
+class TagPostList(ListView):
     template_name = "women/index.html"
     context_object_name = "posts"
     allow_empty = False
 
-    def get_queryset(self):
-        return Women.published.filter(tags__slug=self.kwargs["tag_slug"]).select_related("cat")
-    
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        tag = context["posts"][0].tags.first()
+        tag = TagPost.objects.get(slug=self.kwargs["tag_slug"])
         context["title"] = "Тег - " + tag.tag
-        context["menu"] = menu 
+        context["menu"] = menu
         context["cat_selected"] = None
         return context
+
+    def get_queryset(self):
+        return Women.published.filter(
+            tags__slug=self.kwargs["tag_slug"]
+        ).select_related("cat")
