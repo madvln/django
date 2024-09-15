53d05033d933dea93a412f611e25c46c43842411
madvln <madvln@yandex.ru>
2024-09-15 18:22:50 +0000

    last_lesson

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
 from . import views
 
 app_name = "users"
 
 urlpatterns = [
 
-    path("login/", views.login_user, name="login"), # "users:login"
-    path("logout/", views.logout_user, name="logout"), # "users:logout"
+    path("login/", views.LoginUser.as_view(), name="login"), # "users:login"
+    path("logout/", LogoutView.as_view(), name="logout"), # "users:logout"
 ]
